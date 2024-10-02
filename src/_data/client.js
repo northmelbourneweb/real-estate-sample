316ca8b60472ae00b926dd7c656e7f9d73182ef1
Alan Huynh <hdmquan@outlook.com>
2024-10-03 09:51:30 +1000

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "name": "North Melbourne Web",
+    "email": "design@northmelbourneweb.com",
+    "phoneForTel": "0486-010-043",
+    "phoneFormatted": "+61 (0) 486 010 043",
+    "address": {
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "Lalor",
+        "state": "VIC",
+        "zip": "3075",
+        "mapLink": ""
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://northmelbourneweb.com"
+};
